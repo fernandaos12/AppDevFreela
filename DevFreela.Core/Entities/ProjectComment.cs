23862f52211bdd9a51208d58adc5164e6082b6b8
Fernanda Oliveira <fernandaos12@gmail.com>
2022-12-16 08:14:51 -0300

    Iniciando camadas Biblioteca de classe core

---
+++ DevFreela.Core/Entities/ProjectComment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DevFreela.Core.Entities
+{
+    public class ProjectComment : BaseEntity
+    {
+        public ProjectComment(string content, int idProjet, int idUser)
+        {
+            Content = content;
+            IdProjet = idProjet;
+            IdUser = idUser;
+            CreatedAt = DateTime.Now;
+        }
+
+        public string Content { get; private set; }
+        public int IdProjet { get; private set; }
+        public int IdUser { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+    
+    }
+}
---
+++ DevFreela.Core/Entities/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Entities
+{
+    public class User : BaseEntity
+    {
+        public User(string fullName, string email, DateTime birthDate)
+        {
+            FullName = fullName;
+            Email = email;
+            BirthDate = birthDate;
+
+            CreatedAt = DateTime.Now;
+            Active = true;
+            Skills = new List<UserSkill>();
+            OwnerProject = new List<Project>();
+            FreelanceProject = new List<Project>();
+        }
+
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public Boolean Active { get; set; }
+
+        public List<UserSkill> Skills { get; set; }
+        public List<Project> OwnerProject { get; set; }
+        public List<Project> FreelanceProject { get; set; }
+        
+    }
+}
